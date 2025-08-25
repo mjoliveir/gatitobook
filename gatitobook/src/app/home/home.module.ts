d2f1163f8d563ccb50ea9dc16ad471df6bb6acdf
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-25 12:08:59 -0300

    implementando sharedModule para desacoplamento de codigo

--- gatitobook/src/app/home/home.module.ts
+++ gatitobook/src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -20,8 +21,7 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     CommonModule,
     HomeRoutingModule,
     FormsModule,
-    MensagemModule,
-    ReactiveFormsModule
+    SharedModule
   ],
   exports: [
     HomeComponent
