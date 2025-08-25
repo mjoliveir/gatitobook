830013b8c15b159aa1f65332fc38bf06eec77248
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-25 11:25:00 -0300

    adicionando canLoad na aplicação

--- gatitobook/src/app/app-routing.module.ts
+++ gatitobook/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
+//import { LoginGuard } from './autenticacao/login.guard';
+import { DetalheAnimalComponent } from './animais/detalhe-animal/detalhe-animal.component';
 
 const routes: Routes = [
   {
@@ -11,12 +14,18 @@ const routes: Routes = [
 
   {
     path: 'home',
-    loadChildren:() => import('./home/home.module').then((m) => m.HomeModule)
+    loadChildren:() => import('./home/home.module').then((m) => m.HomeModule),
+    //  canLoad: [LoginGuard]
   },
 
   {
     path: 'animais',
-    loadChildren: () => import('./animais/animais.module').then((m) => m.AnimaisModule)
+    loadChildren: () => import('./animais/animais.module').then((m) => m.AnimaisModule),
+    canLoad:[AutenticacaoGuard]
+  },
+  {
+    path: 'animais/:animalId', //os dois pontos servem para criar um path nao estático, um path dinâmico
+    component: DetalheAnimalComponent
   }
 ];
 
