55cd497f33bba747acb7640123c32c725e4a6811
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-25 06:40:29 -0300

    import do cartaoComponent

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
+import { AnimalComponent } from './animal/animal.component';
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { CartaoModule } from "src/app/componentes/cartao/cartao.module";
 
 
 @NgModule({
   declarations: [
-    ListaAnimaisComponent
+    ListaAnimaisComponent,
+    AnimalComponent,
+    GradeFotosAnimaisComponent
   ],
   imports: [
     CommonModule,
-    AnimaisRoutingModule
-  ]
+    AnimaisRoutingModule,
+    CartaoModule
+]
 })
 export class AnimaisModule { }
