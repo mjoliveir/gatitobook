2b53dd4691ce342e0d9b2b6d4ee07d3b022b8193
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-25 07:45:25 -0300

    pipe async para a grade de fotos dos animais

--- gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Animais } from '../animais';
 import { UsuarioService } from 'src/app/autenticacao/usuario/usuario.service';
 import { AnimaisService } from '../animais.service';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-animais',
@@ -10,15 +12,17 @@ import { AnimaisService } from '../animais.service';
 })
 export class ListaAnimaisComponent implements OnInit {
 
-  animais!: Animais
+  animais$!: Observable<Animais>
 
   constructor(private usuarioService: UsuarioService, private animaisService: AnimaisService) { }
 
   ngOnInit(): void {
-    this.usuarioService.retornaUsuario().subscribe((usuario) => {
-    const userName = usuario.name ?? '';
-    this.animaisService.listaDoUsuario(userName).subscribe((animais) => this.animais = animais)
-    })
+    this.animais$ = this.usuarioService.retornaUsuario().pipe(
+      switchMap((usuario) => {
+        const userName = usuario.name ?? '';
+        return this.animaisService.listaDoUsuario(userName);
+      } )
+    )
   }
 
 }
